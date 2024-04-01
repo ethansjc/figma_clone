99a229f1146bc221fd54003917535e602e0a9fe9
ethansjc <ethansjcheong@gmail.com>
2024-04-01 15:58:09 -0400

    Created starter functionailty for canvas

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,48 @@
-"use client"
+"use client";
 
+import { fabric } from "fabric";
 import Live from "@/components/Live";
 import { Room } from "./Room";
 import Navbar from "@/components/Navbar";
 import LeftSidebar from "@/components/LeftSidebar";
 import RightSidebar from "@/components/RightSidebar";
+import { useEffect, useRef } from "react";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null);
+  const selectedShapeRef = useRef<string | null>('rectangle');
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        selectedShapeRef,
+        shapeRef,
+      });
+    });
+    window.addEventListener("resize", () => {
+      handleResize({ fabricRef });
+    });
+  }, []);
+
   return (
-    <main className="h-screen overflow-hidden">     
+    <main className="h-screen overflow-hidden">
       <Navbar />
       <section className="flex h-full flex-row">
-        <LeftSidebar/>
-        <Live />
-        <RightSidebar/>
+        <LeftSidebar />
+        <Live canvasRef={canvasRef}/>
+        <RightSidebar />
       </section>
     </main>
   );
