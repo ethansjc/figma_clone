2e49cb14c5de33c599739e8572f9734214dea35c
ethansjc <ethansjcheong@gmail.com>
2024-04-01 14:04:24 -0400

    Setup starter components for sidebars

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,16 @@
 import Live from "@/components/Live";
 import { Room } from "./Room";
 import Navbar from "@/components/Navbar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 export default function Page() {
   return (
     <main className="h-screen overflow-hidden">     
       <Navbar />
       <section className="flex h-full flex-row">
+        <LeftSidebar/>
         <Live />
+        <RightSidebar/>
       </section>
     </main>
   );
