594d70cb9e296ab6f4df66d4dd16d7b353daa237
ethansjc <ethansjcheong@gmail.com>
2024-04-03 12:22:29 -0400

    Image functionality now available

--- app/page.tsx
+++ app/page.tsx
@@ -20,6 +20,7 @@ import { ActiveElement } from "@/types/type";
 import { useMutation, useRedo, useStorage, useUndo } from "@/liveblocks.config";
 import { defaultNavElement } from "@/constants";
 import { handleDelete, handleKeyDown } from "@/lib/key-events";
+import { handleImageUpload } from "@/lib/shapes";
 export default function Page() {
   const undo = useUndo();
   const redo = useRedo();
@@ -30,6 +31,7 @@ export default function Page() {
   const shapeRef = useRef<fabric.Object | null>(null);
   const selectedShapeRef = useRef<string | null>(null);
   const activeObjectRef = useRef<fabric.Object | null>(null);
+  const imageInputRef = useRef<HTMLInputElement>(null);
 
   const canvasObjects = useStorage((root) => root.canvasObjects);
 
@@ -83,6 +85,15 @@ export default function Page() {
         setActiveElement(defaultNavElement);
         break;
 
+      case "image":
+        imageInputRef.current?.click();
+        isDrawing.current = false;
+
+        if (fabricRef.current) {
+          fabricRef.current.isDrawingMode = false;
+        }
+        break;
+        
       default:
         break;
     }
@@ -130,13 +141,13 @@ export default function Page() {
     });
 
     window.addEventListener("keydown", (e: any) => {
-      handleKeyDown({ 
-      e,
-      canvas: fabricRef.current,
-      undo,
-      redo,
-      syncShapeInStorage,
-      deleteShapeFromStorage,
+      handleKeyDown({
+        e,
+        canvas: fabricRef.current,
+        undo,
+        redo,
+        syncShapeInStorage,
+        deleteShapeFromStorage,
       });
     });
 
@@ -144,11 +155,9 @@ export default function Page() {
       handleResize({ fabricRef });
     });
 
-   
-
-    return() => {
+    return () => {
       canvas.dispose();
-    }
+    };
   }, []);
 
   useEffect(() => {
@@ -164,6 +173,17 @@ export default function Page() {
       <Navbar
         activeElement={activeElement}
         handleActiveElement={handleActiveElement}
+        imageInputRef={imageInputRef}
+        handleImageUpload={(e: any) => {
+          e.stopPropagation();
+
+          handleImageUpload({
+            file: e.target.files[0],
+            canvas: fabricRef as any,
+            shapeRef,
+            syncShapeInStorage,
+          });
+        }}
       />
       <section className="flex h-full flex-row">
         <LeftSidebar allShapes={Array.from(canvasObjects)} />
